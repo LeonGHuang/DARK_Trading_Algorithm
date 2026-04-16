61ef1f6739de64f7b8043d69baa4f7290ff56539
Leon <l.g.huang02@gmail.com>
2026-04-16 18:05:27 +1200

    add connection to postres isntance on pc because pi instance died

--- src/database/scripts/sql.py
+++ src/database/scripts/sql.py
@@ -5,6 +5,7 @@ import psycopg2
 from dotenv import load_dotenv # variable saved on .env files
 from sqlalchemy import create_engine
 
+
 def engine():
     load_dotenv("/home/leon/projs/dark/gitignore/sql.env", override=True)
     sql_engine = create_engine(f"""
@@ -14,15 +15,31 @@ def engine():
     return sql_engine
 
 
+# old connection to pi, micro sd card died
 def connect():
     load_dotenv("/home/leon/projs/dark/gitignore/sql.env", override=True)
     try:
         conn = psycopg2.connect(
-            host=os.getenv("sqlHOST"),
-            port=os.getenv("sqlPORT"),
-            database=os.getenv("sqlDATABASE"),
-            user=os.getenv("sqlUSER"),
-            password=os.getenv("sqlPASSWORD"),
+            host = os.getenv("sqlHOST"),
+            port = os.getenv("sqlPORT"),
+            database = os.getenv("sqlDATABASE"),
+            user = os.getenv("sqlUSER"),
+            password = os.getenv("sqlPASSWORD"),
+        )
+        return conn
+    except:
+        return sys.exit("connection to sql has failed")
+
+
+def connect_pc():
+    load_dotenv("/home/leon/projs/dark/gitignore/sql.env", override=True)
+    try:
+        conn = psycopg2.connect(
+            host = os.getenv("pc_HOST"),
+            port = os.getenv("pc_PORT"),
+            database = os.getenv("pc_DATABASE"),
+            user = os.getenv("pc_USER"),
+            password = os.getenv("pc_PASSWORD"),
         )
         return conn
     except:
