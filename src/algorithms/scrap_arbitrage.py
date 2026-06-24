6607334b8bf3c7b404ffb80a24a2933973356418
Leon <l.g.huang02@gmail.com>
2026-06-25 03:19:02 +1200

    completed scrap arbitrage, probably should have commited along the way

--- src/algorithms/scrap_arbitrage.py
+++ src/algorithms/scrap_arbitrage.py
@@ -5,99 +5,103 @@ from datetime import datetime, timedelta, timezone
 import numpy as np
 import aiohttp
 import asyncio
+import subprocess
 
 
-def fetch_cost(cursor):
-        query = f"""
-        SELECT * FROM scraping
-        ORDER BY id
-        """
-        cursor.execute(query)
-        return cursor.fetchall()
+class sql_fetch():
+        def __init__(self):
+                self.conn = sql.connect_pc()
+                self.cursor = self.conn.cursor()
 
+        def __exit__(self):
+                self.conn.close()
+                
+        def __del__(self):
+                self.conn.close()
 
-def fetch_items(cursor):
-        query = f"""
-        SELECT 
-        i.id, 
-        i.rarity, 
-        i.inventory_width * i.inventory_height AS size,
-        s.cost_per_slot
-        FROM item_data AS i
-        LEFT JOIN scraping AS s
-        ON i.rarity = s.rarity
-        WHERE type IN ('Armor', 'Weapon')
-        AND i.rarity NOT IN ('Poor', 'Common','Artifact')
-        AND i.rarity = 'Epic'
-        AND i.name NOT ILIKE ALL (SELECT name FROM recipes)
-        ORDER BY id
-        """
-        cursor.execute(query)
-        return cursor.fetchall()
+        def scrap(self, rarity):
+                query = f"""
+                SELECT * FROM scraping
+                WHERE rarity = '{rarity}'
+                ORDER BY id
+                """
+                self.cursor.execute(query)
+                return self.cursor.fetchall()
+
+        def items(self, rarity):
+                query = f"""
+                SELECT 
+                        i.id, 
+                        i.inventory_width * i.inventory_height AS size
+                FROM item_data AS i
+                WHERE type IN ('Armor', 'Weapon')
+                        AND i.rarity = '{rarity}'
+                        AND i.inventory_width * i.inventory_height > 2
+                        AND REPLACE(i.name, '\u2019', '''') NOT IN (SELECT name FROM recipes)
+                ORDER BY id
+                """
+                self.cursor.execute(query)
+                
+                return self.cursor.fetchall()
 
 
 async def fetch_price(ses, sem_limit, item_id):
         async with sem_limit:
                 from_date = (datetime.now(timezone.utc) - timedelta(minutes=60)).strftime("%Y-%m-%dT%H:%M:%SZ")
-                url = f"https://api.darkerdb.com/v1/market?item_id={item_id.replace('\'', "'")}&from={from_date}&limit=50&sold=0"
+                url = f"https://api.darkerdb.com/v1/market?item_id={item_id.replace('\'', "'")}&limit=50&sold=0"
+                # print(url)
                 response =  await ses.get(url)
                 data_body = (await response.json())['body']
                 price_list = [x['price_per_unit'] for x in data_body]
         return price_list
 
+async def scrap_prices(ses, sem_limit, sql_con, rarity):
+        scrap_id, rarity, scraping_cost = sql_con.scrap(rarity)[0]
+        scrap_price = await fetch_price(ses, sem_limit, scrap_id)
 
+        scrap_price = int(np.percentile(scrap_price, 20)) if scrap_price else 99999
+        listing_fee = scrap_price * 0.05
+        processing_cost = listing_fee + scraping_cost
+        # print(scrap_id, rarity, scrap_price, processing_cost)
+        return scrap_id, scrap_price, processing_cost
 
-
-
-async def item_prices(cursor, ses, sem_limit, item_id, rarity, size, cost_per_slot):
+async def item_prices(ses, sem_limit, item_id, size):
         price_list = await fetch_price(ses, sem_limit, item_id)
-        item_cost = int(np.percentile(price_list, 30)) if price_list else 99999
+
+        price_list = sorted(price_list)
+        item_cost = int(np.percentile(price_list, 10)) if price_list else 99999
         price_per_slot = round(item_cost / size)
-        total_cost_per_slot = price_per_slot + cost_per_slot
-        return item_id, rarity, total_cost_per_slot, price_per_slot, item_cost, cost_per_slot
+        # print(item_id, price_per_slot, item_cost, price_list)
+        return [item_id, price_per_slot, item_cost, price_list, size]
 
+async def filter_prices(price_data, scrap_price, processing_cost):
+        for i,x in enumerate(price_data): 
+                price_data[i][1] = x[1] + processing_cost
+        price_data = list(filter(lambda x: x[1] < scrap_price * 1.5, price_data))
+        return price_data
 
-async def scrap_prices(cursor, ses, sem_limit):
-        async def fetch_with_rarity(item_id, rarity):
-                prices = await fetch_price(ses, sem_limit, item_id)
-                return rarity, int(np.mean(prices))
-
-        tasks = [fetch_with_rarity(item_id, rarity) for item_id, rarity, cost_per_slot in fetch_cost(cursor)]
-        scrap_prices = await asyncio.gather(*tasks)
-        return scrap_prices
-
-
-
-
-async def main():
-        conn = sql.connect_pc()
-        cursor = conn.cursor()
-
+async def main(rarity):
+        sql_con = sql_fetch()
         sem_limit = asyncio.Semaphore(50)
 
-        header = [('item_id', 'rarity', 'total_cost_per_slot', 'price_per_slot', 'item_cost', 'cost_per_slot')]
+        header = [('item_id', 'price_per_slot', 'item_cost', 'listings', 'size')]
         async with aiohttp.ClientSession() as ses:
-                tasks = [
-                item_prices(cursor, ses, sem_limit, item_id, rarity, size, scraping_cost)
-                for item_id, rarity, size, scraping_cost in fetch_items(cursor)
-                ]
-                rows = await asyncio.gather(*tasks)
-
-        return header + rows
-
-                # e = await scrap_prices(cursor, ses, sem_limit)
-                # return e
+                scrap_id, scrap_price, processing_cost = await scrap_prices(ses, sem_limit, sql_con, rarity)
+                tasks = [item_prices(ses, sem_limit, item_id, size)
+                        for item_id, size in sql_con.items(rarity)]
+                price_data = await asyncio.gather(*tasks)
+                rows = await filter_prices(price_data, scrap_price, processing_cost)
+        return rarity, scrap_price, header + rows
 
 
-output = asyncio.run(main())
 
 def printing():
-    output[1:] = sorted(output[1:], key=lambda x: (x[2]), reverse=False)
-    for row in output:
-        print(f"{row[1]:<15} {row[0]:<35} {row[2]:>15} {row[3]:>15} {row[4]:>15} {row[5]:>15}")
-
+        rarity, scrap_price, output = asyncio.run(main("Epic"))
+        subprocess.run("clear")
+        print(f"{rarity} Scrap selling at {scrap_price} Gold")
+        output[1:] = sorted(output[1:], key=lambda x: (x[1]), reverse=False)
+        for x in output:
+                print(f"{x[0]:<25} {x[4]:>5} {x[1]:>16} {x[2]:>10} {x[3][0:9]}")
 printing()
-
-
 
 
