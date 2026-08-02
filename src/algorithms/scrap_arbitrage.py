99f1294e910769b9e51493b1e481733422ce7a59
Leon <l.g.huang02@gmail.com>
2026-08-02 16:52:05 +1200

    chore: renamed api key

--- src/algorithms/scrap_arbitrage.py
+++ src/algorithms/scrap_arbitrage.py
@@ -48,7 +48,7 @@ class sql_fetch():
 async def fetch_price(ses, sem_limit, item_id):
         async with sem_limit:
                 from_date = (datetime.now(timezone.utc) - timedelta(minutes=60)).strftime("%Y-%m-%dT%H:%M:%SZ")
-                url = f"https://api.darkerdb.com/v1/market?key={os.getenv('scrap_price_key')}&item_id={item_id.replace('\'', "'")}&limit=50&sold=0"
+                url = f"https://api.darkerdb.com/v1/market?key={os.getenv('scrap_search_key')}&item_id={item_id.replace('\'', "'")}&limit=50&sold=0"
                 # print(url)
                 response =  await ses.get(url)
                 data_body = (await response.json())['body']
