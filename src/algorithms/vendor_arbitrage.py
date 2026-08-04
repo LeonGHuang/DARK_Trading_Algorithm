015f8e6dcba6b0e930945c6854719158d3d1cbde
Leon <l.g.huang02@gmail.com>
2026-08-04 16:37:50 +1200

    feat: updated undercut quantity to appear over total quanity, upated query to use top 60

--- src/algorithms/vendor_arbitrage.py
+++ src/algorithms/vendor_arbitrage.py
@@ -20,24 +20,26 @@ def sql_item():
 	WHERE item_type NOT IN ('armor', 'weapon', 'accessory', 'utility')
 	AND rarity != 'artifact'
 	AND name NOT IN ('Lifeleaf', 'Wardweed', 'Phantom Flower', 'Saltvine', 'Lyre', 'Drum')
+
 	AND name NOT ILIKE '%Key%'
-	AND name NOT ILIKE '%Ingot%'
-	AND name NOT ILIKE '%Ore%'
+	AND name NOT ILIKE '%token%'
+
 	AND id NOT ILIKE '%heart%'
 	AND id NOT ILIKE '%stag%'
-	AND is_tradable = true
-	AND vendor_price > 299
-	ORDER BY vendor_price DESC;
+
+	AND vendor_price IS NOT NULL
+	ORDER BY vendor_price DESC
+	LIMIT 60
         """
         cursor.execute(query)
         return cursor.fetchall()
 
 
-async def fetch(session, conn_limit, stop, id, name, rarity, vendor_price):
+async def fetch(session, conn_limit, rate_limit, id, name, rarity, vendor_price):
 	async with conn_limit:
-		if stop.is_set():                      # ← queued task wakes up, sees the flag, never sends
-            		return None
-		from_date = ( datetime.now(timezone.utc) - timedelta(minutes=30) ).replace(tzinfo=None).isoformat()
+		if rate_limit.is_set():
+			return None
+		from_date = ( datetime.now(timezone.utc) - timedelta(minutes=15) ).replace(tzinfo=None).isoformat()
 		url = f"https://api.darkerdb.com/v2/market?key={os.getenv('vendor_arbitrage_key')}&item_id={id}&from={from_date}&limit=50&sold=0"
 		# print(url)
 
@@ -47,7 +49,7 @@ async def fetch(session, conn_limit, stop, id, name, rarity, vendor_price):
 				output = await response.json()
 		except aiohttp.ClientResponseError as e:
 			print(f"{name}: HTTP {e.status} {e.message} — skipping")
-			stop.set()
+			rate_limit.set()
 			return None
 
 		items                   = [(listing['price_per_unit'], listing['quantity']) for listing in output['body']]
@@ -55,20 +57,20 @@ async def fetch(session, conn_limit, stop, id, name, rarity, vendor_price):
 		extractable_value       = round(sum((vendor_price - x[0]) * x[1] for x in undercut_items))
 		average_undercut_price  = int(np.mean([x[0] for x in undercut_items])) if undercut_items else 0
 		average_margin          = (vendor_price - average_undercut_price) if average_undercut_price else 0
-		undercut_quantity       = round(sum([x[1] for x in undercut_items]))
+		undercut_quantity       = len(undercut_items)
+		total_quantity		= len(items)
 
-		return (name, extractable_value, vendor_price, average_margin, undercut_quantity, undercut_items)
-
+		return (id, extractable_value, vendor_price, average_margin, f'{undercut_quantity}/{total_quantity:>2}', undercut_items)
 
 async def main():
-	header = [('name', 'extractable', 'vendor_price', 'avg_margin', 'quantity', 'listings')]
+	header = [('name', 'extractable', 'vendor_price', 'avg_margin', 'undercut', 'listings')]
 	sem = asyncio.Semaphore(10)
-	stop = asyncio.Event()
+	rate_limit = asyncio.Event()
 	start_time = time.perf_counter()
 	target_items = sql_item()
 	async with aiohttp.ClientSession() as session:
 		task = [
-		fetch(session, sem, stop, id, name, rarity, vendor_price)
+		fetch(session, sem, rate_limit, id, name, rarity, vendor_price)
 		for id, name, rarity, vendor_price in target_items
 		]
 		rows = await asyncio.gather(*task)
@@ -85,4 +87,4 @@ if __name__ == "__main__":
 	# rows[1:] = filter(lambda x: x[1], rows[1:])
 	rows[1:] = sorted(rows[1:], key=lambda x: x[1], reverse=True)
 	for x in rows:
-		print(f"{x[0]:<32} {x[1]:>15} {x[2]:>15} {x[3]:>15} {x[4]:>15}")
+		print(f"{x[0]:<40} {x[1]:>15} {x[2]:>15} {x[3]:>15} {f'{x[4]}':>15}")
