eb76b86edb4636c1a288c7d63c4c9eafae9b31e8
Leon <l.g.huang02@gmail.com>
2026-08-11 02:15:28 +1200

    feat: added other merchants crafting output info

--- src/database/table_creation/vendor_output.py
+++ src/database/table_creation/vendor_output.py
@@ -15,6 +15,7 @@ def table_deletion(cursor):
 def table_creation(cursor):
         query = """
         CREATE TABLE IF NOT EXISTS vendor_craftout(
+                merchant_id TEXT,
                 item_id TEXT,
                 name TEXT
                 )
@@ -24,25 +25,36 @@ def table_creation(cursor):
 def table_entry(cursor, rows):
         query = """
         INSERT INTO vendor_craftout (
+                merchant_id,
                 item_id,
                 name
         )
-        VALUES (%s, %s)
+        VALUES (%s, %s, %s)
         """
         cursor.executemany(query, rows)
 
 
-def url():
-        url = f'https://api.darkerdb.com/v2/merchants/id.merchant.alchemist?key={os.getenv("general")}'
+def url(merchant_id):
+        url = f'https://api.darkerdb.com/v2/merchants/{merchant_id}?key={os.getenv("general")}'
         print(url)
         return url
 
+def merchant_list(cursor):
+        query = """
+        SELECT id FROM vendor_info
+        WHERE 'craft' = ANY(service_types);
+        """
+        cursor.execute(query)
+        merchant_ids = cursor.fetchall()
+        return merchant_ids
 
-def fetch_data():
-        fetch_data = requests.get(url()).json()['body']['craft']
-        sql_rows = [(item.get('item_id'), item.get('name')) for item in fetch_data]
-        # print(sql_rows)
-        return sql_rows
+def fetch_data(cursor):
+        body_list = []
+        for merchant in merchant_list(cursor):
+                craft = requests.get(url(merchant[0])).json()['body']['craft']
+                sql_rows = [(merchant[0], item.get('item_id'), item.get('name')) for item in craft]
+                body_list.extend(sql_rows)
+        return body_list
  
 
 if __name__=="__main__":
@@ -53,6 +65,7 @@ if __name__=="__main__":
 
         table_deletion(cursor)
         table_creation(cursor)
-        table_entry(cursor, fetch_data())
+        rows = fetch_data(cursor)
+        table_entry(cursor, rows)
 
         conn.commit()
