fb20242bf93a1894ee4b3ed21947eb2a3e3dc372
Leon <l.g.huang02@gmail.com>
2026-08-11 19:08:56 +1200

    feat: initial commit for vendor craft input table creation

---
+++ src/database/table_creation/vendor_input.py
@@ -0,0 +1,81 @@
+import requests
+from dotenv import load_dotenv
+import os
+import src.database.scripts.sql as sql
+import subprocess
+
+load_dotenv("/home/leon/projs/dark/.env/api.env")
+
+def table_deletion(cursor):
+        query = """
+        DROP TABLE IF EXISTS vendor_craftin
+        """
+        cursor.execute(query)
+
+def table_creation(cursor):
+        query = """
+        CREATE TABLE IF NOT EXISTS vendor_craftin(
+                merchant_id TEXT,
+                item_id TEXT,
+                component_item_id TEXT,
+                component_name TEXT,
+                component_amount INT
+                )
+        """
+        cursor.execute(query)
+
+def table_entry(cursor, rows):
+        query = """
+        INSERT INTO vendor_craftin (
+                merchant_id,
+                item_id,
+                component_item_id,
+                component_name,
+                component_amount
+        )
+        VALUES (%s, %s, %s, %s, %s)
+        ON CONFLICT DO NOTHING """
+        cursor.executemany(query, rows)
+
+
+def url(merchant_id):
+        url = f'https://api.darkerdb.com/v2/merchants/{merchant_id}?key={os.getenv("general")}'
+        return url
+
+def merchant_list(cursor):
+        query = """
+        SELECT id FROM vendor_info
+        WHERE 'craft' = ANY(service_types);
+        """
+        cursor.execute(query)
+        merchant_ids = cursor.fetchall()
+        return merchant_ids
+
+def fetch_data(cursor):
+        body_list = []
+        for merchant in merchant_list(cursor):
+                craft = requests.get(url(merchant[0])).json()['body']['craft']
+                for item in craft:
+                        sql_rows = [(
+                                merchant[0],
+                                item.get('item_id'),
+                                component.get('item_id'),
+                                component.get('name'),
+                                component.get('amount')
+                        ) for component in item.get('materials')]
+                        body_list.extend(sql_rows)
+        return body_list
+
+
+if __name__=="__main__":
+        subprocess.run('clear')
+
+        conn = sql.connect_pod()
+        cursor = conn.cursor()
+
+        table_deletion(cursor)
+        table_creation(cursor)
+        rows = fetch_data(cursor)
+        table_entry(cursor, rows)
+
+        conn.commit()
